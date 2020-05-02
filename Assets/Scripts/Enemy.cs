cece381be4b54aa08b281cab86c4e024703a1ffb
OleG1ad <gladenkov.gov@gmail.com>
2020-05-02 20:56:55 +0300

    Week 5 | Session 4

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Vector3 center = new Vector3(0, 0, 0)
+
+if(Vector3.Distance(gameObject.transform.position), center) > centerDistanceLimit))
+{
+       Vector3 centerDirection = (center - transform.position).normalized;
+enemyRb.AddForce(centerDirection* speed);
+} 
+else 
+{
+      Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+enemyRb.AddForce(lookDirection* speed);
+}*/
+
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+
+        enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
